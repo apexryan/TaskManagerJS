0b37f6f3f4ba89689fd3a947961e955f7e1d0b4e
apexryan <bymythdzn@gmail.com>
2024-10-17 02:28:43 +0530

    datetime added

--- tm.js
+++ tm.js
@@ -8,6 +8,22 @@ function addtask() {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
 
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear();
+    let hours = currentDate.getHours();
+    let ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = String(hours).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    const dateString = `${day}/${month}/${year} , ${hours}:${minutes} ${ampm}`;
+
+    let dateSpan = document.createElement("dtspan");
+    dateSpan.innerHTML = ` ( ${dateString} )`;
+    dateSpan.className = "date-time";
+    li.appendChild(dateSpan);
+
     let editButton = document.createElement("edit-btn");
     editButton.className = "edit-btn";
     editButton.onclick = function () {
